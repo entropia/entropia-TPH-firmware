1db1fa13f35bdcb52157c2a6267d33e669d75785
GityUpNow <kim-cheng@gmx.de>
2018-12-13 22:05:31 +0100

    Integrated control-topics into MQTT.hpp

--- src/MQTT.hpp
+++ src/MQTT.hpp
@@ -4,11 +4,27 @@
 #include <Adafruit_MQTT.h>
 #include <Adafruit_MQTT_Client.h>
 
-#define MQTT_SERVER      "mqtt.club.entropia.de"
-#define MQTT_SERVER_PORT 1883
-#define MQTT_TOPIC       "/public/sensoren/TPH/foo"
-#define MQTT_USER        "eve"
-#define MQTT_PASSWORD    "entropia"
+#define MQTT_PROCESS_TIME 10
+
+//Server-Settings
+#define MQTT_SERVER          "mqtt.club.entropia.de"
+#define MQTT_SERVER_PORT     1883
+#define MQTT_PREFIX          "/public/sensoren/TPH/"
+
+//Device/Access-Settings
+#define MQTT_DEVICE_NAME     "foo"
+#define MQTT_USER            "eve"
+#define MQTT_PASSWORD        "entropia"
+
+//Not required by default
+//Concatination
+#define MQTT_TOPIC           MQTT_PREFIX MQTT_DEVICE_NAME
+#define MQTT_CONTROL_TOPIC   MQTT_TOPIC "/control"
+#define MQTT_RESPONSE_TOPIC  MQTT_TOPIC "/response"
+
+#define MQTT_COMMON_TOPIC    MQTT_PREFIX "common"
+#define MQTT_COMMON_CONTROL  MQTT_COMMON_TOPIC "/control"
+#define MQTT_COMMON_RESPONSE MQTT_COMMON_TOPIC "/response"
 
 extern Adafruit_MQTT_Client mqtt;
 
